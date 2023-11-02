b0c9dd6d1744033d48f21d9772451460b851e9af
Mykal Coleman <mycoleman.art@gmail.com>
2023-11-02 11:55:13 -0700

    Goode enough for now

--- sketch.js
+++ sketch.js
@@ -1,22 +1,73 @@
+let apiURL = `http://api.weatherapi.com/v1/current.json?key=31cccdf7e57341dc9ae170022230211&q=94542&aqi=yes`;
+
+let weatherData; // Variable to store the weather data
+let animationSpeed = 2;
+let lineY;
+let lineX; // X-coordinate for the line
+
 function setup() {
   createCanvas(600, 800);
-  background(100);
-  let url = 'http://api.weatherapi.com/v1/current.json?key=31cccdf7e57341dc9ae170022230211&q=94587&aqi=yes';
-  loadJSON(url, gotWeather);
+  background(50);
+
+  lineY = height / 2;
+  lineX1 = 0; // Initialize the X-coordinate for the line
+  lineX2 = -50; // Initialize the X-coordinate for the line
+  lineX3 = 100; // Initialize the X-coordinate for the line
+
+  setInterval(updateWeather, 200); // Update weather every 30 minutes (in milliseconds)
+  updateWeather(); // Initial update
 }
 
-function gotWeather(weather) {
-  // Extract city and state information from the JSON data
-  let cityName = weather.location.name;
-  let stateName = weather.location.region;
+function updateWeather() {
+  loadJSON(apiURL, gotWeather);
+}
 
-  // Display city and state information on the canvas
-  textSize(16);
-  textAlign(CENTER);
-  fill(255); // Set text color to white
-  text(`City: ${cityName}`, width / 2, height / 2 - 20);
-  text(`State: ${stateName}`, width / 2, height / 2 + 20);
+function gotWeather(data) {
+  // Store the weather data in the global variable
+  weatherData = data;
 }
 
 function draw() {
+  background(50);
+  // Check if weatherData is available and not undefined
+  if (weatherData) {
+    let cityName = weatherData.location.name;
+    let stateName = weatherData.location.region;
+    let precipIn = weatherData.current.precip_in;
+    let windMph = weatherData.current.wind_mph;
+    let feelslikeF = weatherData.current.feelslike_f;
+    let conditionText = weatherData.current.condition.text; // Added to get the condition text
+
+    // Display city, state, precipitation, wind speed, feels like, and condition on the canvas
+    textSize(32);
+    textAlign(CENTER);
+    fill(255); // Set text color to white
+    text(`${cityName}, ${conditionText}, ${feelslikeF} (°F)`, width / 2, 75);
+    text(`Wind Speed (mph): ${windMph}`, width / 2, height / 2 + 200);
+
+    // Update the X-coordinate for the line based on animationSpeed
+    lineX1 += windMph;
+    lineX2 += windMph + 0.5;
+    lineX3 += windMph - 0.5;
+
+    // Wrap the line around the screen
+    if (lineX1 > width + 100) {
+      lineX1 = -100;
+    }
+
+    if (lineX2 > width + 100) {
+      lineX2 = -100;
+    }
+
+    if (lineX3 > width + 100) {
+      lineX3 = -100;
+    }
+
+    // Wind animation
+    fill(255);
+    noStroke();
+    rect(lineX1, lineY, 85, 2);
+    rect(lineX2, lineY + 50, 100, 2);
+    rect(lineX3, lineY - 25, 145, 2);
+  }
 }
