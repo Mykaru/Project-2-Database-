3ee2db116c4b8188176e0cc907bc6a0f891270bc
Mykal Coleman <mycoleman.art@gmail.com>
2023-11-06 16:43:25 -0800

    live updates

--- sketch.js
+++ sketch.js
@@ -1,30 +1,49 @@
-let apiURL = `https://api.weatherapi.com/v1/current.json?key=31cccdf7e57341dc9ae170022230211&q=94542&aqi=yes`;
-
+let apiURL;
 let weatherData;
 let animationSpeed = 2;
 let lineY;
-let lineX; 
+let lineX1;
+let lineX2;
+let lineX3;
 
 function setup() {
   createCanvas(600, 600);
   background(50);
 
+  var form = document.getElementById("cityForm");
+  let locationInput = select('#locationInput');
+  form.addEventListener('submit', handleForm);
+  function handleForm(event) {
+    event.preventDefault(); 
+    var newLocation = locationInput.value();
+    updateLocation(newLocation);
+  }
+  
+  
   lineY = height / 2;
-  lineX1 = 0; 
-  lineX2 = -50; 
+  lineX1 = 0;
+  lineX2 = -50;
   lineX3 = 100;
 
-  setInterval(updateWeather, 200);
-  updateWeather();
+  // Initialize with default location
+  updateLocation('hayward');
+
 }
 
-function updateWeather() {
+function updateLocation(newLocation) {
+  console.log('Updating location with:', newLocation);
+  apiURL = `https://api.weatherapi.com/v1/current.json?key=31cccdf7e57341dc9ae170022230211&q=${newLocation}&aqi=yes`;
   loadJSON(apiURL, gotWeather);
+  console.log('Updated location:', locationInput.value);
 }
 
 function gotWeather(data) {
   weatherData = data;
 }
+
+// Your drawing and animation code goes here
+
+
 
 function draw() {
   
@@ -37,20 +56,21 @@ function draw() {
     let conditionText = weatherData.current.condition.text;
     let isDay = weatherData.current.is_day;
 
-    if (conditionText === "Sunny" && isDay === 1) {
-      setGradient(0, 0, width, height, color(0, 0, 255), color(135, 206, 250));
-
-    } else if (conditionText === "Cloudy" && isDay === 1) {
-      setGradient(0, 0, width, height, color(192), color(192)); 
-
-    } else if ((conditionText === "Sunny" || conditionText === "Clear") && isDay === 0) {
+    if (isDay === 0) {
+      // Night sky gradient
       setGradient(0, 0, width, height, color(0, 0, 20), color(15, 5, 40));
-
-    } else if (conditionText === "Cloudy" && isDay === 0) {
-      setGradient(0, 0, width, height, color(5, 5, 10), color(20, 20, 30));
-
     } else {
-      background(50);
+      // Daytime conditions
+      if (conditionText === "Sunny" || conditionText === "Clear") {
+        setGradient(0, 0, width, height, color(0, 0, 255), color(135, 206, 250)); // Sunny day
+      } else if (conditionText === "Cloudy" || conditionText === "Light rain") {
+        setGradient(0, 0, width, height, color(192), color(100)); // Cloudy day
+      } else if (conditionText === "Partly cloudy") {
+        setGradient(0, 0, width, height, color(135, 206, 235), color(192, 192, 192)); // Partly cloudy day
+      } else {
+        // Default background for unknown conditions
+        background(50);
+      }
     }
 
     
